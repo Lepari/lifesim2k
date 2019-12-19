0f619907747e8b5954899eb82cf3efe30df24acb
Kalle Lyttinen <k.lyttinen@gmail.com>
2019-12-19 11:59:19 +0000

    added basic grid of buttons as base of ui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,24 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    m_size = 9;
+    const QVector<QToolButton *> initVector(m_size, nullptr);
+    m_buttons = QVector<QVector<QToolButton *> >(m_size, initVector);
+    QGridLayout *layout = new QGridLayout(this);
+
+    for (int i = 0; i < m_size; i++) {
+        for (int j = 0; j < m_size; j++) {
+            QToolButton *button = new QToolButton(this);
+            button->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+            layout->addWidget(button, i, j );
+            m_buttons[i][j] = button;
+            m_buttons[i][j]->setText("a");
+            m_buttons[i][j]->setEnabled(true);
+        }
+    }
+    QWidget *central = new QWidget(0,0);
+    central->setLayout(layout);
+    setCentralWidget(central);
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,11 +2,16 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QToolButton>
+#include <QGridLayout>
+
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
+private:
+    QVector<QVector<QToolButton *> > m_buttons;
+    int m_size;
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
