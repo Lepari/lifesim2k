bbf9a1efb51b7fb72b75eb10132c9a807111291c
Kalle Lyttinen <k.lyttinen@gmail.com>
2020-01-02 11:03:06 +0000

    refactored widgets as members

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,45 +4,66 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     m_size = 9;
-    const QVector<QToolButton *> initVector(m_size, nullptr);
-    m_buttons = QVector<QVector<QToolButton *> >(m_size, initVector);
-    QGridLayout *grid_layout = new QGridLayout(this);
-    QVBoxLayout *top_layout = new QVBoxLayout(this);
-    QHBoxLayout *button_layout = new QHBoxLayout(this);
-    top_layout->addItem(grid_layout);
-    top_layout->addItem(button_layout);
+    m_central = new QWidget(0,0);
+    m_top_layout = new QVBoxLayout(this);
+    m_grid_layout = new QGridLayout(this);
+    m_button_layout = new QHBoxLayout(this);
+    m_top_layout->addItem(m_grid_layout);
+    m_top_layout->addItem(m_button_layout);
 
     //Lower row control buttons setup
     QPushButton *tick_button = new QPushButton("Tick",this);
     QPushButton *start_stop_button = new QPushButton("Start");
     QLabel *size_label = new QLabel("size", this);
     QSpinBox *size_spinbox = new QSpinBox(this);
+
+
+
     size_spinbox->setValue(9);
     size_spinbox->setMinimum(1);
     size_spinbox->setMaximum(200);
-    button_layout->addWidget(tick_button);
-    button_layout->addWidget(start_stop_button);
-    button_layout->addWidget(size_label);
-    button_layout->addWidget(size_spinbox);
+    m_button_layout->addWidget(tick_button);
+    m_button_layout->addWidget(start_stop_button);
+    m_button_layout->addWidget(size_label);
+    m_button_layout->addWidget(size_spinbox);
+
+    populateLifeButtons();
+    setCentralWidget(m_central);
 
 
-    //Populate the grid of buttons that represent life
+    m_central->setLayout(m_top_layout);
+    connect(size_spinbox, SIGNAL(valueChanged(int)), this, SLOT(sizeChanged(int)));
+}
+
+MainWindow::~MainWindow()
+{
+}
+
+void MainWindow::populateLifeButtons()
+{
+    const QVector<QToolButton *> initVector(m_size, nullptr);
+    m_buttons = QVector<QVector<QToolButton *> >(m_size, initVector);
+
+//TODO remove all widgets from grid before adding new ones
+
     for (int i = 0; i < m_size; i++) {
         for (int j = 0; j < m_size; j++) {
             QToolButton *button = new QToolButton(this);
             button->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-            grid_layout->addWidget(button, i, j );
+            m_grid_layout->addWidget(button, i, j );
             m_buttons[i][j] = button;
             m_buttons[i][j]->setText("a");
             m_buttons[i][j]->setEnabled(true);
         }
     }
 
-    QWidget *central = new QWidget(0,0);
-    central->setLayout(top_layout);
-    setCentralWidget(central);
+
 }
 
-MainWindow::~MainWindow()
+void MainWindow::sizeChanged(int value)
 {
+    m_size = value;
+    populateLifeButtons();
+    m_central->repaint();
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -14,9 +14,17 @@
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+public slots:
+    void sizeChanged(int value);
+
 private:
     QVector<QVector<QToolButton *> > m_buttons;
+    QWidget* m_central;
     int m_size;
+    void populateLifeButtons();
+    QGridLayout *m_grid_layout;
+    QVBoxLayout *m_top_layout;
+    QHBoxLayout *m_button_layout;
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
